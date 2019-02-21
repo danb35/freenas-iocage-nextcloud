159a10af67c23a1374b509d50404d5e611739c7f
Jacopo Nasi <jacopo.nasi@gmail.com>
2019-02-21 19:49:43 +0100

    Adding imagick PHP module
    Adding the missing, highly suggested by Nextcloud, module for PHP72

--- nextcloud-jail.sh
+++ nextcloud-jail.sh
@@ -128,7 +128,7 @@ cat <<__EOF__ >/tmp/pkg.json
   "php72-session","php72-wddx","php72-xsl","php72-filter",
   "php72-curl","php72-fileinfo","php72-bz2","php72-intl","php72-openssl",
   "php72-ldap","php72-ftp","php72-imap","php72-exif","php72-gmp",
-  "php72-memcache","php72-opcache","php72-pcntl","php72","bash","perl5",
+  "php72-memcache","php72-opcache","php72-pcntl", "php72-pecl-imagick", "php72","bash","perl5",
   "p5-Locale-gettext","help2man","texinfo","m4","autoconf","socat","git","apache24"
   ]
 }

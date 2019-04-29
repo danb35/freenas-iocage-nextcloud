0d2b7542fb27e5448b16b73c9aa43336500198e6
Dan Brown <dan@familybrown.org>
2019-04-29 11:18:07 -0400

    Update to Nextcloud 16

--- nextcloud-jail.sh
+++ nextcloud-jail.sh
@@ -175,8 +175,8 @@ iocage exec ${JAIL_NAME} chown -R www:www /mnt/files
 iocage exec ${JAIL_NAME} chmod -R 770 /mnt/files
 iocage exec ${JAIL_NAME} "if [ -z /usr/ports ]; then portsnap fetch extract; else portsnap auto; fi"
 iocage exec ${JAIL_NAME} chsh -s /usr/local/bin/bash root
-iocage exec ${JAIL_NAME} fetch -o /tmp https://download.nextcloud.com/server/releases/latest-15.tar.bz2
-iocage exec ${JAIL_NAME} tar xjf /tmp/latest-15.tar.bz2 -C /usr/local/www/apache24/data/
+iocage exec ${JAIL_NAME} fetch -o /tmp https://download.nextcloud.com/server/releases/nextcloud-16.0.0.tar.bz2
+iocage exec ${JAIL_NAME} tar xjf /tmp/nextcloud-16.0.0.tar.bz2 -C /usr/local/www/apache24/data/
 iocage exec ${JAIL_NAME} chown -R www:www /usr/local/www/apache24/data/nextcloud/
 iocage exec ${JAIL_NAME} sysrc apache24_enable="YES"
 if [ "${DATABASE}" = "mariadb" ]; then

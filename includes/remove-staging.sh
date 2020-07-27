d4537ac073c7583dbd2e897ea8157a95bcc27b62
Dan Brown <dan@familybrown.org>
2020-07-27 07:33:34 -0400

    rc file already had reload

--- includes/remove-staging.sh
+++ includes/remove-staging.sh
@@ -1,5 +1,5 @@
-#!/usr/local/bin/bash
+#!/bin/sh
 # Remove acme-staging CA from Caddyfile, so Caddy gets production certs
 
 sed -ri.bak 's/acme_ca/#acme_ca/' /usr/local/www/Caddyfile
-service caddy restart
+service caddy reload

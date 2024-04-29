531f9380a3a30967fcf98b3fd4e3cde4c6f6c811
Dan <danb35@gmail.com>
2024-04-29 06:24:20 -0400

    Update nextcloud-jail.sh
    Install PHP 8.3 rather than 8.2.

--- nextcloud-jail.sh
+++ nextcloud-jail.sh
@@ -224,42 +224,42 @@ cat <<__EOF__ >/tmp/pkg.json
     "texinfo",
     "m4",
     "autoconf",
-    "php82",
-    "php82-ctype",
-    "php82-curl",
-    "php82-dom",
-    "php82-filter",
-    "php82-gd",
-    "php82-xml",
-    "php82-mbstring",
-    "php82-posix",
-    "php82-session",
-    "php82-simplexml",
-    "php82-xmlreader",
-    "php82-xmlwriter",
-    "php82-zip",
-    "php82-zlib",
-    "php82-fileinfo",
-    "php82-bz2",
-    "php82-intl",
-    "php82-ldap",
-    "php82-pecl-smbclient",
-    "php82-ftp",
-    "php82-imap",
-    "php82-bcmath",
-    "php82-gmp",
-    "php82-exif",
-    "php82-pecl-APCu",
-    "php82-pecl-memcache",
-    "php82-pecl-redis",
-    "php82-pecl-imagick",
-    "php82-pcntl",
-    "php82-phar",
-    "php82-iconv",
-    "php82-sodium",
-    "php82-sysvsem",
-    "php82-xsl",
-    "php82-opcache"
+    "php83",
+    "php83-ctype",
+    "php83-curl",
+    "php83-dom",
+    "php83-filter",
+    "php83-gd",
+    "php83-xml",
+    "php83-mbstring",
+    "php83-posix",
+    "php83-session",
+    "php83-simplexml",
+    "php83-xmlreader",
+    "php83-xmlwriter",
+    "php83-zip",
+    "php83-zlib",
+    "php83-fileinfo",
+    "php83-bz2",
+    "php83-intl",
+    "php83-ldap",
+    "php83-pecl-smbclient",
+    "php83-ftp",
+    "php83-imap",
+    "php83-bcmath",
+    "php83-gmp",
+    "php83-exif",
+    "php83-pecl-APCu",
+    "php83-pecl-memcache",
+    "php83-pecl-redis",
+    "php83-pecl-imagick",
+    "php83-pcntl",
+    "php83-phar",
+    "php83-iconv",
+    "php83-sodium",
+    "php83-sysvsem",
+    "php83-xsl",
+    "php83-opcache"
   ]
 }
 __EOF__
@@ -333,9 +333,9 @@ iocage exec "${JAIL_NAME}" chmod -R 770 /mnt/files
 #####
 
 if [ "${DATABASE}" = "mariadb" ]; then
-  iocage exec "${JAIL_NAME}" pkg install -y mariadb106-server php82-pdo_mysql php82-mysqli
+  iocage exec "${JAIL_NAME}" pkg install -y mariadb106-server php83-pdo_mysql php83-mysqli
 elif [ "${DATABASE}" = "pgsql" ]; then
-  iocage exec "${JAIL_NAME}" pkg install -y postgresql13-server php82-pgsql php82-pdo_pgsql
+  iocage exec "${JAIL_NAME}" pkg install -y postgresql13-server php83-pgsql php83-pdo_pgsql
 fi
 
 # Ports not currently used, Commented out for future use
